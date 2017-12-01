98da3098a043dd044988e98e37827a09bce3df39
Robert Zhang <Lei.Zhang@microsoft.com>
2017-12-01 17:43:35 +0800

    Add basic top bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
