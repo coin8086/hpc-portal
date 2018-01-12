743dde775417373b0eda105c459edfd33d205863
Robert Zhang <Lei.Zhang@microsoft.com>
2018-01-12 15:57:22 +0800

    Show test result with mock data.

--- src/app/diagnostics/result/result.component.ts
+++ src/app/diagnostics/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -7,12 +8,15 @@ import { Location } from '@angular/common';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  private id: number;
 
   constructor(
+    private route: ActivatedRoute,
     private location: Location
   ) {}
 
   ngOnInit() {
+    this.id = + this.route.snapshot.paramMap.get('id');
   }
 
   goBack(): void {
