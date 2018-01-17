e07e103fdea8fef4813e16ad03ea8f8c431c89a2
Robert Zhang <Lei.Zhang@microsoft.com>
2018-01-17 17:04:28 +0800

    Add icons for main nav items, link h1 title to home and refactor app component code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private items = [
+    {
+      link: 'dashboard',
+      title: 'Dashboard',
+      icon: 'dashboard',
+    },
+    {
+      link: 'resource',
+      title: 'Resource',
+      icon: 'cloud',
+    },
+    {
+      link: 'diagnostics',
+      title: 'Diagnostics',
+      icon: 'local_hospital',
+    },
+    {
+      link: 'command',
+      title: 'Cluster Run',
+      icon: 'call_to_action',
+    },
+  ];
 }
