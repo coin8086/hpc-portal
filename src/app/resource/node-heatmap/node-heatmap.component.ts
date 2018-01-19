44135c3557c028b489eff77b62cf4fb7f1be3e1c
Robert Zhang <Lei.Zhang@microsoft.com>
2018-01-19 11:12:13 +0800

    Add simple node heat map.

--- src/app/resource/node-heatmap/node-heatmap.component.ts
+++ src/app/resource/node-heatmap/node-heatmap.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'resource-node-heatmap',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./node-heatmap.component.css']
 })
 export class NodeHeatmapComponent implements OnInit {
+  @Input() nodes = [];
+  @Output() clickNode: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  nodeClass(node): string {
+    let res;
+    if (node.runningJobs < 10)
+      res = 'low';
+    else if (node.runningJobs < 50)
+      res = 'median';
+    else
+      res = 'high';
+    return res;
+  }
+
+  nodeTip(node): string {
+    return `${node.name}: ${node.runningJobs} jobs`;
+  }
+
+  clickRow(node): void {
+    this.clickNode.emit(node);
+  }
 }
