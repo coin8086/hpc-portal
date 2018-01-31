b5ef317b46246ecc46bb85fadcfe93de3bc70f5e
Robert Zhang <Lei.Zhang@microsoft.com>
2018-01-31 11:48:40 +0800

    More mock info for ping test result.

--- src/app/diagnostics/in-memory-data.service.ts
+++ src/app/diagnostics/in-memory-data.service.ts
@@ -65,11 +65,6 @@ export class InMemoryDataService implements InMemoryDbService {
   }
 
   generatePingTest(id) {
-    //Node Error Count Host Node(s) Name
-    //IAASCN001 TimedOut 3 IaaSCN000,IaaSCN002,ref
-    //IAASCN002 TimedOut 3 IaaSCN000,IaaSCN001,ref
-    //IAASCN000 TimedOut 3 IaaSCN001,IaaSCN002,ref
-
     let result = {
       id: id,
       testName: 'Ping Test',
@@ -77,113 +72,76 @@ export class InMemoryDataService implements InMemoryDbService {
       progress: 1.0,
       startedAt: now - 1000 * 60 * 28,
       updatedAt: now - 1000 * 60 * 27,
-      summary: {
-        pings: [
-          {
-            node: 'IAASCN001',
-            error: 'TimedOut',
-            count: 3,
-            nodes: 'IaaSCN000,IaaSCN002,ref',
-          },
-          {
-            node: 'IAASCN002',
-            error: 'TimedOut',
-            count: 3,
-            nodes: 'IaaSCN000,IaaSCN001,ref',
-          },
-          {
-            node: 'IAASCN000',
-            error: 'TimedOut',
-            count: 3,
-            nodes: 'IaaSCN001,IaaSCN002,ref',
-          },
-        ]
-      }
     } as Result;
 
     let names = this.generateNames(100);
     result.nodes = names.map(name => {
-      let best = Math.round(Math.random() * 10);
-      let worst = best + Math.round(Math.random() * 100);
-      let avg = Math.round((best + worst) / (2 + Math.random()));
+      let ok = Math.random() < 0.85;
+
+      //Destination IP Address Result Average Best Worst Successful Failures
+      //IAASCN000 fe80::eda3:a138:14ab:a23e Succeeded 0 ms 0 ms 0 ms 4 0
+      //IAASCN001  Failed 0 ms 0 ms 0 ms 0 4
+      //IAASCN002  Failed 0 ms 0 ms 0 ms 0 4
+      //REF 10.0.0.5 Succeeded 0 ms 0 ms 0 ms 4 0
+
+      let pings = names.map(pname => {
+        if (name == pname) {
+          return {
+            destination: pname,
+            ip: 'xxx.xxx.xxx.xxx',
+            result: 'success' ,
+            average: 0,
+            best: 0,
+            worst: 0,
+            successful: 4,
+            failed: 0,
+          };
+        }
+        let ok2 = ok || Math.random() < 0.2;
+        let best, worst, avg;
+        if (ok2) {
+          best = Math.round(Math.random() * 10);
+          worst = best + Math.round(Math.random() * 100);
+          avg = Math.round((best + worst) / (2 + Math.random()));
+        }
+        else {
+          best = 0;
+          worst = 0;
+          avg = 0;
+        }
+        return {
+          destination: pname,
+          ip: 'xxx.xxx.xxx.xxx',
+          result: ok2 ? 'success' : 'failure',
+          average: avg,
+          best: best,
+          worst: worst,
+          successful: ok2 ? 4 : 0,
+          failed: ok2 ? 0 : 4,
+        };
+      });
+
+      let best, worst, avg;
+      if (ok) {
+        best = Math.round(Math.random() * 10);
+        worst = best + Math.round(Math.random() * 100);
+        avg = Math.round((best + worst) / (2 + Math.random()));
+      }
+      else {
+        best = 0;
+        worst = 0;
+        avg = 0;
+      }
+
       return {
         name: name,
-        state: Math.random() < 0.8 ? 'success' : 'failure',
-
-        /*
-         * The "best", "worst" and "avarage" should be calculated from
-         * the "pings" section. They're here for simplicity of demo.
-         * */
+        state: ok ? 'success' : 'failure',
         best: best,
         worst: worst,
         average: avg,
 
         details: {
-          //Destination IP Address Result Average Best Worst Successful Failures
-          //IAASCN000 fe80::eda3:a138:14ab:a23e Succeeded 0 ms 0 ms 0 ms 4 0
-          //IAASCN001  Failed 0 ms 0 ms 0 ms 0 4
-          //IAASCN002  Failed 0 ms 0 ms 0 ms 0 4
-          //REF 10.0.0.5 Succeeded 0 ms 0 ms 0 ms 4 0
-
-          pings: [
-            {
-              destination: 'IAASCN000',
-              ip: 'fe80::eda3:a138:14ab:a23e',
-              result: 'Succeeded' ,
-              average: 0,
-              best: 0,
-              worst: 0,
-              successful: 4,
-              failed: 0,
-            },
-            {
-              destination: 'IAASCN001',
-              ip: null,
-              result: 'Failed',
-              average: 0,
-              best: 0,
-              worst: 0,
-              successful: 0,
-              failed: 4,
-            },
-            {
-              destination: 'IAASCN002',
-              ip: null,
-              result: 'Failed',
-              average: 0,
-              best: 0,
-              worst: 0,
-              successful: 0,
-              failed: 4,
-            },
-            {
-              destination: 'REF',
-              ip: '10.0.0.5',
-              result: 'Succeeded',
-              average: 0,
-              best: 0,
-              worst: 0,
-              successful: 4,
-              failed: 0,
-            },
-          ],
-
-          //Destination Message Count
-          //IAASCN001 TimedOut 4
-          //IAASCN002 TimedOut 4
-
-          failedPings: [
-            {
-              destination: 'IAASCN001',
-              message: 'TimedOut',
-              count: 4,
-            },
-            {
-              destination: 'IAASCN002',
-              message: 'TimedOut',
-              count: 4,
-            },
-          ],
+          pings: pings,
         },
       };
     });
