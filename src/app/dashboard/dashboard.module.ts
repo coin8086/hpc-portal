1a5a97aed2067e3e1842bcb0d193cfa1e41685ad
Robert Zhang <Lei.Zhang@microsoft.com>
2018-01-31 15:46:34 +0800

    Add skeleton code of dashboard components.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NodesComponent } from './nodes/nodes.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
   ],
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, NodesComponent, JobsComponent, LocationComponent],
   //bootstrap: [HomeComponent]
 })
 export class DashboardModule { }
---
+++ src/app/dashboard/nodes/nodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-nodes',
+  templateUrl: './nodes.component.html',
+  styleUrls: ['./nodes.component.css']
+})
+export class NodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
